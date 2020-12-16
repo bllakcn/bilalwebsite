5380910a2d79c521e0d1501df7867778f265f25f
bllakcn <bllakcn35@gmail.com>
2020-12-17 00:06:14 +0100

    Viz.vue added

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -14,28 +14,17 @@
         </div>
       </div>
     </div>
-    <!--
-    <div class="block p-5 border rounded-lg">
-      <h1 class="pb-4 px-5 font-pop">&bull; Visualizations</h1>
-      <div class="block md:flex">
-        <div v-for="record in records" :key="record" class="pb-10 md:p-5">
-          <div class="bwcss rounded-lg border shadow hover:shadow-inner">
-            <router-link :to="'/portfolio/' + record.fields.slug" class="overflow-auto">
-              <img :src="record.fields.image[0].url" class="w-auto mb-5 rounded-lg shadow">
-              <h1 class="pl-4 pb-1 font-chivo text-lg">{{record.fields.title}}</h1>
-              <h2 class="pl-4 pb-1 mb-5 font-libre tracking-widest text-gray-700 text-xs ">{{record.fields.date}}</h2>
-          </router-link>
-          </div>
-        </div>
-      </div>
-    </div>
-    -->
+    <app-viz></app-viz>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+  import axios from 'axios'
+  import viz from './Viz'
   export default {
+    components: {
+      "app-viz":viz
+    },
     data() {
       return {
         apiUrl: 'https://api.airtable.com/v0/',
---
+++ src/views/Viz.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="block p-5 border rounded-lg">
+      <h1 class="pb-4 px-5 font-pop">&bull; Visualizations</h1>
+      <div class="block md:flex md:flex-wrap">
+        <div v-for="record in records" :key="record" class="pb-10 md:p-5 w-6/12">
+          <div class="bfcss">
+            <img :src="record.fields.image[0].url" alt="">
+            <a :href="record.fields.link">
+              <h2 class="pl-4 pb-1 mb-5 font-libre tracking-widest text-gray-700 text-xs">{{record.fields.date}}</h2>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import axios from 'axios'
+  export default {
+data() {
+      return {
+      index: null,
+        apiUrl: 'https://api.airtable.com/v0/',
+        apiKey: 'keypShAsE2JBGmmi3',
+        base: 'appeMm88M1T3Jjzq9/Table%201',
+        records: [],
+        loading: false,
+      }
+    },
+    methods: {
+      getData(){
+        axios({
+          url: this.apiUrl + this.base,
+          headers: {
+            'Authorization': `Bearer ${this.apiKey}`
+          }
+        }).then((res) => {
+        this.records = res.data.records
+        setTimeout(() => {this.loading = true},500)
+          })
+      },
+    },
+    mounted () {
+      this.getData();
+    },
+  }
+
+</script>
+
+<style>
+  .bfcss{
+    transition: filter .6s;
+    filter: grayscale(60%);
+  }
+  .bfcss:hover{
+    filter: grayscale(0);
+    transform: scale(1.005);
+  }
+</style>
